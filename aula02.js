65a58140d6b2ad88655109389ce51be353275ceb
marcos0305 <carvalho.oliveira0305@gmail.com>
2025-09-08 16:27:16 -0300

    Curso de Typescript Completo #P02 Entendo ar formas de compilar o Typescript para gerar o Javascript

---
+++ aula02.js
@@ -0,0 +1,6 @@
+var nome = "Marcos";
+var sobreNome = "Paulo";
+var curso = "TypeScript";
+console.log(nome);
+console.log(sobreNome);
+console.log(curso);
---
+++ aula02.ts
@@ -0,0 +1,7 @@
+const nome = "Marcos";
+const sobreNome = "Paulo";
+const curso = "TypeScript"
+
+console.log(nome);
+console.log(sobreNome);
+console.log(curso);
