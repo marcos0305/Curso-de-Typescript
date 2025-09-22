881d3eadffde4ffdef682e9c0fc065c5375b801b
marcos0305 <carvalho.oliveira0305@gmail.com>
2025-09-22 20:00:13 -0300

    Curso de Typescript Completo #P04 Entendendo como funciona Type Inference em Typescript

---
+++ build/aula04.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name;
+name = "Marcos";
+name = "Paulo";
+let value;
+value = 0;
+value = 10, 5;
+console.log(value);
+console.log(name);
+//# sourceMappingURL=aula04.js.map
---
+++ src/aula04.ts
@@ -0,0 +1,10 @@
+let name: string;
+name = "Marcos"; 
+name = "Paulo";
+
+let value: number;
+value= 0; 
+value = 10,5;
+
+console.log(value);
+console.log(name)
