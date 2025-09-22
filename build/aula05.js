9dbced2495c28688c5bf06a61828f7d8e323ae7c
marcos0305 <carvalho.oliveira0305@gmail.com>
2025-09-22 20:46:38 -0300

    Curso de Typescript Completo #P05 Entendendo como funciona UNION TYPES em Typescript

---
+++ build/aula05.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//let test: number | string;
+let test;
+test = "Marcos";
+test = "35";
+let course = ["JAVA", "JS", "TS", 100, true];
+let value = [10, 100, 1000];
+value.push(3000);
+course.push("ANGULAR");
+console.log(value);
+console.log(course);
+console.log(test);
+//# sourceMappingURL=aula05.js.map
---
+++ src/aula05.ts
@@ -0,0 +1,14 @@
+//let test: number | string;
+let test: any;
+test = "Marcos";
+test = "35";
+
+let course = ["JAVA", "JS", "TS", 100, true];
+let value = [10,100,1000];
+
+value.push(3000);
+course.push("ANGULAR");
+
+console.log(value);
+console.log(course);
+console.log(test);
