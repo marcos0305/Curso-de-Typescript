09123d2212936e7b9c6da2b126c4fbb864e3f52a
marcos0305 <carvalho.oliveira0305@gmail.com>
2026-01-12 19:45:33 -0300

    #P11 Aprendendo sobre Type Assertion e Typecast em Typescript

---
+++ build/aula11.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nvalor;
+let svalor;
+let uvalor;
+uvalor = true;
+svalor = "20";
+nvalor = Number.parseInt(svalor);
+console.log(typeof (nvalor));
+console.log(nvalor);
+/*nvalor=<number> uvalor;
+svalor=<string> uvalor;
+
+ console.log(typeof(uvalor))
+console.log(uvalor)
+
+ console.log(typeof(nvalor))
+console.log(nvalor)
+
+ console.log(typeof(svalor))
+console.log(svalor)*/
+//# sourceMappingURL=aula11.js.map
---
+++ src/aula11.ts
@@ -0,0 +1,23 @@
+let nvalor: number;
+let svalor: string;
+let uvalor: unknown;
+
+uvalor = true;
+svalor= "20";
+
+nvalor=Number.parseInt(svalor);
+
+ console.log(typeof(nvalor))
+console.log(nvalor)
+
+/*nvalor=<number> uvalor;
+svalor=<string> uvalor;
+
+ console.log(typeof(uvalor))
+console.log(uvalor)
+
+ console.log(typeof(nvalor))
+console.log(nvalor)
+
+ console.log(typeof(svalor))
+console.log(svalor)*/
