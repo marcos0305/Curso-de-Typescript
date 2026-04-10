188ad24b3d818a6e05c5850d13ec27c76a61851d
marcos0305 <carvalho.oliveira0305@gmail.com>
2026-04-10 16:10:00 -0300

    Desestruturação em Typescript #P2 Curso de Typescript Completo #P36

--- build/aula35.js
+++ build/aula35.js
@@ -8,8 +8,12 @@ const cor = {
     cor4: "vermelho",
 };
 let { cor1, cor2, cor3, cor4 } = cor;
-console.log(cor1);
-console.log(cor2);
-console.log(cor3);
-console.log(cor4);
+let [nu1, nu2 = 0, ...nu3] = [10, 20, 30, 40, 50, 60, 70];
+const cores = () => {
+    return ["branco", "verde", "amarelo", "vermelho"];
+};
+let [c1, c2, c3, c4] = cores();
+let texto = "hello world";
+let [...t] = texto.split(" ");
+console.log(t);
 //# sourceMappingURL=aula35.js.map
